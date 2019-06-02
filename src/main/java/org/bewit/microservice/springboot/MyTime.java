9f52762ffbfb4400bc4c70716b4d02fd9139fd37
zhangbin19920902 <zhagnbin126126@126.com>
2019-06-02 10:17:23 +0800

    2019.6.2 10:13

---
+++ src/main/java/org/bewit/microservice/springboot/MyTime.java
@@ -0,0 +1,13 @@
+package org.bewit.microservice.springboot;
+
+public class MyTime {
+    private Long currentTime;
+
+    public Long getCurrentTime() {
+        return currentTime;
+    }
+
+    public void setCurrentTime(Long currentTime) {
+        this.currentTime = currentTime;
+    }
+}

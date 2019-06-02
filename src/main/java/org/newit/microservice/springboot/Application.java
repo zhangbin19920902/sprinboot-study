5be78ace31695eab6b1c5681a8de04a09129bd3e
zhangbin19920902 <zhagnbin126126@126.com>
2019-06-02 10:24:00 +0800

    change banner

---
+++ src/main/java/org/newit/microservice/springboot/Application.java
@@ -0,0 +1,39 @@
+package org.newit.microservice.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+@Controller
+public class Application {
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        return "Hello word";
+    }
+
+    @RequestMapping("/index")
+    @ResponseBody
+    public String index(){
+        return "index";
+    }
+
+    @RequestMapping("/currentTime")
+    @ResponseBody
+    public MyTime currentTime(){
+        MyTime myTime = new MyTime();
+        myTime.setCurrentTime(System.currentTimeMillis());
+        return myTime;
+
+    }
+
+    public static void main(String[] args){
+        SpringApplication.run(Application.class);
+    }
+
+
+
+}
